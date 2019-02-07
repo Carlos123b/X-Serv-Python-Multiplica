3119f0794abb7dcbdfc1f56011c23bc44ff966c9
Carlos123b <barrosocarlos98@gmail.com>
2019-02-07 18:36:07 +0100

    Subo archivo al repositorio

---
+++ mult.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+for i in range(1,11):
+    print("Tabla del " + str(i) + ":")
+    print("---------------------------")
+    for j in range(1,11):
+        print(i,"por",j,"es",i*j)
+    print("\n")
